fb2117cbd68eb47ee40a3d90ba039cf822cab876
Darren <darren.m.hassan@gmail.com>
2019-04-21 11:40:00 -0700

    Add SelfCrossing solution.

--- SelfCrossing.java
+++ SelfCrossing.java
@@ -5,6 +5,7 @@ import org.junit.runner.notification.Failure;
 
 import java.util.Arrays;
 
+import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
 /**
@@ -38,6 +39,11 @@ import static junit.framework.TestCase.assertTrue;
  */
 public class SelfCrossing {
 
+    private static class Coord {
+        public int x;
+        public int y;
+    }
+
     public boolean isSelfCrossing(int[] x) {
         if (x == null || x.length < 4) {
             return false;
@@ -45,18 +51,76 @@ public class SelfCrossing {
         if (Arrays.stream(x).anyMatch(i -> i < 0)) {
             return false;
         }
-        int directionIdx = 0;
-        int[] directionSum = new int[4];
-        for (int i = 0; i < x.length; i++) {
-            directionSum[directionIdx++] += x[i];
-            if (directionIdx >= directionSum.length) {
-                directionIdx = 0;
+        int n = x.length;
+        for (int i = n - 1; i > 1; i--) {
+            Coord line1Start = findCoord(x, i);
+            Coord line1End = findCoord(x, i - 1);
+
+            for (int j = i - 2; j >= 0; j--) {
+
+                Coord line2Start = findCoord(x, j);
+                Coord line2End = findCoord(x, j - 1);
+
+                if (linesCross(line1Start, line1End, line2Start, line2End)) {
+                    return true;
+                }
             }
         }
-        if (directionSum[0] >= directionSum[2] && directionSum[1] <= directionSum[3]) {
-            return true;
-        }
         return false;
+    }
+
+    private boolean linesCross(Coord line1Start,
+                               Coord line1End,
+                               Coord line2Start,
+                               Coord line2End) {
+        boolean line1Vert = line1Start.x == line1End.x;
+        boolean line2Vert = line2Start.x == line2End.x;
+        if (line1Vert && line2Vert) {
+            return line1Start.x == line2Start.x &&
+                    ((line1Start.y >= line2Start.y && line1Start.y <= line2End.y) ||
+                    (line1Start.y >= line2End.y && line1Start.y <= line2Start.y));
+        } else if (!line1Vert && !line2Vert) {
+            return line1Start.y == line2Start.y &&
+                    ((line1Start.x >= line2Start.x && line1Start.x <= line2End.x) ||
+                    (line1Start.x >= line2End.x && line1Start.x <= line2Start.x));
+        } else if (line1Vert) {
+            return ((line1Start.y >= line2Start.y && line1End.y <= line2Start.y) ||
+                    (line1End.y >= line2Start.y && line1Start.y <= line2Start.y)) &&
+                    ((line1Start.x >= line2Start.x && line1Start.x <= line2End.x) ||
+                    (line1Start.x >= line2End.x && line1Start.x <= line2Start.x));
+        } else {
+            return ((line2Start.y >= line1Start.y && line2End.y <= line1Start.y) ||
+                    (line2End.y >= line1Start.y && line2End.y <= line1Start.y)) &&
+                    ((line2Start.x >= line1Start.x && line2Start.x <= line1End.x) ||
+                    (line2Start.x >= line1End.x && line2Start.x <= line1Start.x));
+        }
+    }
+
+    private Coord findCoord(int[] x, int index) {
+        Coord coord = new Coord();
+        int[] sign = new int[] {1 , -1, -1, 1};
+        int signCnt = 0;
+        for (int i = 0; i <= index; i++) {
+            if (i % 2 == 0) {
+                coord.y += (x[i] * sign[signCnt++]);
+            } else {
+                coord.x += (x[i] * sign[signCnt++]);
+            }
+            signCnt = signCnt >= sign.length ? 0 : signCnt;
+        }
+        return coord;
+    }
+
+    @Test
+    public void testCase4() {
+        SelfCrossing solution = new SelfCrossing();
+        assertFalse(solution.isSelfCrossing(new int[] {3, 3, 4, 2, 2}));
+    }
+
+    @Test
+    public void testCase3() {
+        SelfCrossing solution = new SelfCrossing();
+        assertFalse(solution.isSelfCrossing(new int[] {1, 2, 3, 4}));
     }
 
     @Test
