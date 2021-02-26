e6968b472da76418d64d0e5f565fd44158c53937
Darren <darren.m.hassan@gmail.com>
2021-02-26 15:47:06 -0800

    Palindrome partitioning 3 fixed.

--- recursion/PalindromePartitioning3.java
+++ recursion/PalindromePartitioning3.java
@@ -1,24 +1,31 @@
 package recursion;
 
 /**
+ * Problem 1278
  * Source: https://leetcode.com/problems/palindrome-partitioning-iii/
  */
 public class PalindromePartitioning3 {
+
+    public static void main(String[] arc) {
+        PalindromePartitioning3 sol = new PalindromePartitioning3();
+        String testCase = "faaglagedtwnejzpuarkgwgoefwra";
+        System.out.println(sol.palindromePartition(testCase, 27));
+    }
     public int palindromePartition(String s, int k) {
-        if (k > s.length()) {
+        if (k >= s.length()) {
             return 0;
         }
         return partition(s, k);
     }
     int partition(String s, int k) {
-        if (k == 1) {
+        if (s.length() == 1 || k == 1) {
             return makePalindrome(s);
         }
         int minRet = 100000;
         for (int i = 1; i < s.length(); i++) {
             int ret = 0;
-            ret = makePalindrome(s.substring(0, i));
-            ret += partition(s.substring(i, s.length()), k--);
+            ret = partition(s.substring(0, i), 1);
+            ret += partition(s.substring(i, s.length()), k-1);
             minRet = Math.min(ret, minRet);
         }
         return minRet;
@@ -31,7 +38,7 @@ public class PalindromePartitioning3 {
         int ret = 0;
         int mid = s.length() / 2;
         char[] beg = s.substring(0, mid).toCharArray();
-        char[] end = s.substring(mid + 1, s.length() -1).toCharArray();
+        char[] end = s.substring(mid, s.length()).toCharArray();
         for (int i = 0; i < beg.length; i++) {
             if (beg[i] != end[end.length - 1 - i]) {
                 ret++;
