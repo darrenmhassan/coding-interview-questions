eaf25861dd47e8ea246aeeb4d98169fa6f10504b
Darren <darren.m.hassan@gmail.com>
2021-03-28 14:44:38 -0700

    Find common characters.

---
+++ FindCommonCharacters.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class FindCommonCharacters {
+    public List<String> commonChars(String[] A) {
+
+        List<String> sortedStrs = new ArrayList<>();
+        for (int i = 0; i < A.length; i++) {
+            String s = A[i];
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            sortedStrs.add(new String(c));
+        }
+
+        List<String> ret = new ArrayList<>();
+        for (int i = 0; i < sortedStrs.get(0).length(); i++) {
+            char firstStrC = sortedStrs.get(0).charAt(i);
+            boolean charPresent = true;
+            for (int j = 1; j < sortedStrs.size() && charPresent; j++) {
+                for (int k = 0; k < sortedStrs.get(j).length(); k++) {
+                    if (k == sortedStrs.get(j).length() -1
+                            && firstStrC != sortedStrs.get(j).charAt(k)) {
+                        charPresent = false;
+                    } else if (firstStrC == sortedStrs.get(j).charAt(k)) {
+                        char[] s = sortedStrs.get(j).toCharArray();
+                        StringBuilder sb = new StringBuilder();
+                        for (int l = 0; l < s.length; l++) {
+                            if (l != k) {
+                                sb.append(s[l]);
+                            }
+                        }
+                        sortedStrs.set(j, sb.toString());
+                        break;
+                    }
+                }
+            }
+            if (charPresent) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(firstStrC);
+                ret.add(sb.toString());
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] argc) {
+        String[] input = {"bella","label","roller"};
+        FindCommonCharacters sol = new FindCommonCharacters();
+        List<String> ret = sol.commonChars(input);
+        ret.stream().forEach(s -> System.out.println(s));
+    }
+}
--- FindSolutionForEquation.java
+++ FindSolutionForEquation.java
@@ -7,17 +7,17 @@ import java.util.List;
  * Source: https://leetcode.com/problems/find-positive-integer-solution-for-a-given-equation/
  */
 public class FindSolutionForEquation {
-    public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
-        List<List<Integer>> ret = new ArrayList<>();
-        for (int x = 1; x < 1001; x++) {
-            for (int y = 1; y < 1001; y++) {
-                int zz = customfunction.f(x, y);
-                if (zz == z) {
-                    List<Integer> res = Arrays.asList(x, y);
-                    ret.add(res);
-                }
-            }
-        }
-        return ret;
-    }
+//    public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
+//        List<List<Integer>> ret = new ArrayList<>();
+//        for (int x = 1; x < 1001; x++) {
+//            for (int y = 1; y < 1001; y++) {
+//                int zz = customfunction.f(x, y);
+//                if (zz == z) {
+//                    List<Integer> res = Arrays.asList(x, y);
+//                    ret.add(res);
+//                }
+//            }
+//        }
+//        return ret;
+//    }
 }
